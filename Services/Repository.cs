ce3faca69bdb0cc6fc30f70134c399229ac4d063
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-21 19:50:57 +0100

    Servicios

---
+++ Services/Repository.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.DAServices.Servicios
+{
+    public class Repository<T> : IDisposable where T : class
+    {
+        private readonly UsersContext context;
+
+        public Repository(UsersContext context)
+        {
+            this.context = context;
+        }
+
+        protected DbSet<T> EntitySet
+        {
+            get{
+                return context.Set<T>();
+            }
+        }
+
+          public async Task<IEnumerable<T>> GetObjet()
+        {
+            return await EntitySet.ToListAsync();
+        }
+
+        public async Task<T> GetObjetByID(int id)
+        {
+            return await EntitySet.FindAsync(id);
+        }
+
+        public async Task<T> InsertObjet(T objeto)
+        {
+            EntitySet.Add(objeto);
+            await Save();
+            return objeto;
+        }
+
+        public async Task<T> DeleteObjet(int id)
+        {
+            T objeto = await EntitySet.FindAsync(id);
+            EntitySet.Remove(objeto);
+            await Save();
+            return objeto;
+        }
+
+        public async Task<T> UpdateObjet(T objeto)
+        {
+            context.Entry(objeto).State = EntityState.Modified;
+            await Save();
+            return objeto;
+        }
+
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
